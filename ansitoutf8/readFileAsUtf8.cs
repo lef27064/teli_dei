83a523fcd2d1db9ffdcde91500b30a857ee27b38
lef27064 <lef27064@otenet.gr>
2017-02-20 08:57:36 +0200

    Add files via upload

---
+++ ansitoutf8/readFileAsUtf8.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.IO; 
+
+
+namespace ansiToUTF8
+{
+    public class AnsiToUtf8
+    {
+        public static string readFileAsUtf8(string fileName)
+      {
+            Encoding encoding = Encoding.Default;
+            String original = String.Empty;
+
+            using (StreamReader sr = new StreamReader(fileName, Encoding.Default))
+            {
+                original = sr.ReadToEnd();
+                encoding = sr.CurrentEncoding;
+                sr.Close();
+            }
+
+            if (encoding == Encoding.UTF8)
+                return original;
+
+            byte[] encBytes = encoding.GetBytes(original);
+            byte[] utf8Bytes = Encoding.Convert(encoding, Encoding.UTF8, encBytes);
+            return Encoding.UTF8.GetString(utf8Bytes);
+        }
+    }
+}
